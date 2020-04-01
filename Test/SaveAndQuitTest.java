5ee95d21b02d196eb5942d1a5ba307cd23cc83ca
SG <sgarabediancenternode@gmail.com>
2020-04-01 14:06:56 +0200

    Add first tests in plan test

---
+++ Test/SaveAndQuitTest.java
@@ -0,0 +1,7 @@
+public class SaveAndQuitTest {
+
+    //List is kept as is when appropriate function is chosen
+
+    //Programs terminates
+
+}
---
+++ Test/SortItemsTest.java
@@ -0,0 +1,9 @@
+public class SortItemsTest {
+
+    //List default sorting is by ID
+
+    //List can be sorted by Date - Descending
+
+    //List can be sorted by project - alphabetically
+
+}
