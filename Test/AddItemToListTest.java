bccb46cf967b92059db0bf3fa1ca79d865d97c45
S.G <43292329+SaraGarabedian@users.noreply.github.com>
2020-04-01 14:08:58 +0200

    Add first tests in plan test (#1)
    Co-authored-by: SG <sgarabediancenternode@gmail.com>

---
+++ Test/AddItemToListTest.java
@@ -0,0 +1,6 @@
+public class AddItemToListTest {
+
+    //Check that a new item can be added to a list
+
+    //The main menu counter for Tasks to do is changed accordingly
+}
---
+++ Test/UpdateListTest.java
@@ -0,0 +1,15 @@
+public class UpdateListTest {
+
+    //Changing title
+
+    //Changing due date
+
+    //Changing Project
+
+    //Mark as Done works
+
+    //Mark as done changes the main menu counter for tasks that are done
+
+    //Delete works: removes a task
+
+}
