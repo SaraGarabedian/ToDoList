859ab96f34c32e826772cb78fee6732c10125729
SG <sgarabediancenternode@gmail.com>
2020-04-22 10:23:50 +0200

    Delete hard-coded list (Main.java) Create loadFromFile method (FileUtil.java) Add constructor with 4 arguments (Task.java)

--- src/FileUtil.java
+++ src/FileUtil.java
@@ -1,15 +1,13 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileUtil {
     public static final String fileName = "src\\TaskList.txt";
 
     public static void writeToFile (List<Task> taskList) throws IOException {
         File file = new File(fileName);
-        System.out.println(file.getAbsolutePath());
+        //System.out.println(file.getAbsolutePath());
         FileWriter write = new FileWriter(file, false);
         PrintWriter printLine = new PrintWriter(write);
         for (Task task : taskList) {
@@ -17,5 +15,22 @@ public class FileUtil {
         }
         printLine.close();
         write.close();
+    }
+
+    public static void loadFromFile(List<Task> taskList) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner fileReader = new Scanner(file);
+        while (fileReader.hasNextLine()){
+            String currentLine = fileReader.nextLine();
+
+            String[] splitLine = currentLine.split("\\|");
+            String title = splitLine[0].trim();
+            String dueDate = splitLine[1].trim();
+            String project = splitLine[2].trim();
+            String status = splitLine[3].trim();
+
+            Task task = new Task(title, dueDate, project, status);
+            taskList.add(task);
+        }
     }
 }
